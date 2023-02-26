42ad618a4bbdc24c5b0696ff7a6d47282d416c2f
L1daLobanova <124993574+L1daLobanova@users.noreply.github.com>
2023-02-26 22:20:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,12 +14,16 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   uint64_t k = 0;
-  for (uint64_t i = 2; i <= 100000; i++){
+  uint64_t m = 0;
+  for (uint64_t i = 2; i <= 100000; i++) {
     if (checkPrime(i) == true)
-      k+=1;
-    if (k == n)
-      return i;
+      k += 1;
+    if (k == n) {
+      m = i;
+      break;
+    }
   }
+  return m;
 }
 
 uint64_t nextPrime(uint64_t value) {
